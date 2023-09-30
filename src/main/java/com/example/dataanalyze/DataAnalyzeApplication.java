67bbeba24a86b8891f37cb92f3cdcd030120e3f6
yolkaSD <yolka.sd@gmail.com>
2023-09-30 20:28:26 +0700

    first commit

---
+++ src/main/java/com/example/dataanalyze/DataAnalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataanalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalyzeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dataanalyze/DataAnalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataanalyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
