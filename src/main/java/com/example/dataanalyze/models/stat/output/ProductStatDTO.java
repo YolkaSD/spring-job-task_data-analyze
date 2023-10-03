2871050e74f2466c88518f6d2a01de6648c60fae
yolkaSD <yolka.sd@gmail.com>
2023-10-03 20:06:35 +0700

    сделал правку в структуре представления выходного json

--- src/main/java/com/example/dataanalyze/models/stat/output/ProductStatDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.dataanalyze.models.stat.output;
-
-import lombok.Data;
-
-@Data
-public class ProductStatDTO {
-    private String name;
-    private double expenses;
-}
--- src/main/java/com/example/dataanalyze/models/stat/output/PurchasesDTO.java
+++ src/main/java/com/example/dataanalyze/models/stat/output/PurchasesDTO.java
@@ -2,11 +2,9 @@ package com.example.dataanalyze.models.stat.output;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class PurchasesDTO {
     private String name;
-    private List<ProductStatDTO> expenses;
+    private double expenses;
 
 }
