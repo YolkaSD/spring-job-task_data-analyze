90e4d1314b60fb46955aeb1023937c6b0939e48d
yolkaSD <yolka.sd@gmail.com>
2023-10-03 19:03:43 +0700

    Добавлена зависимость для работы с датами JSON (jackson-datatype-jsr310)

--- src/main/java/com/example/dataanalyze/services/write/OutputWriter.java
+++ src/main/java/com/example/dataanalyze/services/write/OutputWriter.java
@@ -1,6 +1,7 @@
 package com.example.dataanalyze.services.write;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Component;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,12 +11,13 @@ import java.io.IOException;
  *
  */
 @Component
-public class SearchWriter implements Writable {
+public class OutputWriter implements Writable {
 
 
     @Override
     public void write(Object dto, String outputPath) {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
         try (FileWriter fileWriter = new FileWriter(outputPath)) {
             mapper.writeValue(fileWriter, dto);
         } catch (IOException e) {
