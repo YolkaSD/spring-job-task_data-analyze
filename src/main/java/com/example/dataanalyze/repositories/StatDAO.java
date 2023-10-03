805daca27aa51b335fed2caf8b6828d9cb8965e2
yolkaSD <yolka.sd@gmail.com>
2023-10-03 23:34:06 +0700

    сделан  запрос для получения списка товаров, купленных за определенный период конкретным покупателем

--- src/main/java/com/example/dataanalyze/repositories/StatDAO.java
+++ src/main/java/com/example/dataanalyze/repositories/StatDAO.java
@@ -1,10 +1,14 @@
 package com.example.dataanalyze.repositories;
 
 
+import com.example.dataanalyze.models.stat.input.StatInput;
 import com.example.dataanalyze.models.stat.output.CustomerStatDTO;
+import com.example.dataanalyze.models.stat.output.PurchasesDTO;
 
 import java.util.List;
 
 public interface StatDAO {
-    List<CustomerStatDTO> executeGetName();
+    List<CustomerStatDTO> executeGetNames();
+
+    List<PurchasesDTO> executeGetPurchases(CustomerStatDTO customer, StatInput inputDate);
 }
--- src/main/java/com/example/dataanalyze/repositories/StatDaoImpl.java
+++ src/main/java/com/example/dataanalyze/repositories/StatDaoImpl.java
@@ -1,9 +1,14 @@
 package com.example.dataanalyze.repositories;
 
+import com.example.dataanalyze.models.stat.input.StatInput;
 import com.example.dataanalyze.models.stat.output.CustomerStatDTO;
+import com.example.dataanalyze.models.stat.output.PurchasesDTO;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -16,11 +21,44 @@ public class StatDaoImpl implements StatDAO{
     }
 
     @Override
-    public List<CustomerStatDTO> executeGetName() {
+    public List<CustomerStatDTO> executeGetNames() {
         return jdbcTemplate.query("select first_name, last_name from sa.customers", (rs, rowNum) -> {
             CustomerStatDTO customerStatDTO = new CustomerStatDTO();
             customerStatDTO.setName(rs.getString("first_name") + " " + rs.getString("last_name"));
             return customerStatDTO;
         });
+    }
+
+    @Override
+    public List<PurchasesDTO> executeGetPurchases(CustomerStatDTO customer, StatInput inputDate) {
+        String sql = "SELECT DISTINCT " +
+                "    pr.product_name AS name, " +
+                "    SUM(pr.price) AS expenses " +
+                "FROM " +
+                "    sa.customers c " +
+                "    JOIN sa.purchases pu ON c.customer_id = pu.customer_id " +
+                "    JOIN sa.products pr ON pu.product_id = pr.product_id " +
+                "WHERE " +
+                "    c.first_name = ? AND c.last_name = ? " +
+                "    AND pu.purchase_date BETWEEN ? AND ? " +
+                "GROUP BY " +
+                "    pr.product_name " +
+                "ORDER BY " +
+                "    SUM(pr.price); ";
+
+
+        String fullName = customer.getName();
+        String[] nameParts = fullName.split(" ");
+
+        Object[] args = {nameParts[0], nameParts[1], inputDate.getStartDate(), inputDate.getEndDate()};
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+
+            PurchasesDTO purchasesDTO = new PurchasesDTO();
+            purchasesDTO.setName(rs.getString("name"));
+            purchasesDTO.setExpenses(rs.getDouble("expenses"));
+            return purchasesDTO;
+        }, args);
+
     }
 }
